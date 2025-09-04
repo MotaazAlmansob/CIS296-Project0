c124873ff84887011138f8b98cdd1cc1b8214e98
MotaazAlmansob <malmansob03@gmail.com>
2025-09-04 18:32:35 -0400

    Wrote code

--- CIS-296-Project0/src/Main.java
+++ CIS-296-Project0/src/Main.java
@@ -2,14 +2,18 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        String nameAndPronouns   = "Hassan Kdouh (he/him)";
+        String major             = "Software Engineering";
+        String priorExperience   = "C, C++, Python, SQL";
+        String funFact           = "I like sports";
+        String coolestTechThing  = "Assist doctors with diagnosing disease";
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        System.out.println("Classmate Profile");
+        System.out.println("-----------------");
+        System.out.println("Name & Pronouns: " + nameAndPronouns);
+        System.out.println("Major: " + major);
+        System.out.println("Previous Programming Experience: " + priorExperience);
+        System.out.println("Fun Fact: " + funFact);
+        System.out.println("Most Interesting Tech Thing Seen: " + coolestTechThing);
     }
 }
